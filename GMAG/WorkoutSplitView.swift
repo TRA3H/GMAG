3f85389ab668293de740b9338a9bee27877e0a00
Cyrus Baybay <41849568+TRA3H@users.noreply.github.com>
2023-10-27 04:07:45 -0700

    Update WorkoutSplitView.swift

--- GMAG/WorkoutSplitView.swift
+++ GMAG/WorkoutSplitView.swift
@@ -1,70 +1,50 @@
-//
-//  WorkoutSplitView.swift
-//  GMAG
-//
-//  Created by Abdon Jesus Baybay on 10/27/23.
-//
-
 import SwiftUI
 
 struct WorkoutSplitView: View {
-    var body: some View {
-        VStack {
-            // Week Display
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
-                    ForEach(DayOfWeek.allCases, id: \.self) { day in
-                        Text(day.rawValue)
-                            .frame(width: 100, height: 100)
-                            .background(Color.gray.opacity(0.2))
-                            .cornerRadius(10)
-                            .padding()
-                    }
-                }
-            }
-            
-            // Workout Split Card Component
-            WorkoutSplitCard(splitName: "Chest / Tri", workouts: ["Bench Press", "Tri Pull downs"])
-            
-            // Add Workout Split Button
-            Button(action: {
-                // TODO: Add functionality to create a new workout split card
-            }) {
-                Text("Add Workout Split")
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
-        }
-    }
-}
-
-struct WorkoutSplitCard: View {
-    var splitName: String
-    var workouts: [String]
+    @State private var scrollOffset: CGFloat = 0
+    private let cardWidth: CGFloat = 250
+    private let cardHeight: CGFloat = 500
+    private let spacing: CGFloat = 30
     
     var body: some View {
-        VStack {
-            Text(splitName)
-                .font(.headline)
-                .padding(.top)
-            
-            ForEach(workouts, id: \.self) { workout in
-                Text(workout)
+        GeometryReader { fullView in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: spacing) {
+                    ForEach(DayOfWeek.allCases, id: \.self) { day in
+                        GeometryReader { geometry in
+                            Text(day.rawValue)
+                                .frame(width: cardWidth, height: cardHeight)
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(10)
+                                .zIndex(self.zIndex(for: geometry.frame(in: .global).minX, in: fullView.size.width))
+                        }
+                        .frame(width: cardWidth, height: cardHeight)
+                    }
+                }
+                .padding(.horizontal, (fullView.size.width - cardWidth) / 2)
+                .offset(x: self.scrollOffset)
+                .gesture(DragGesture().onEnded { value in
+                    self.handleScrollEnd(with: fullView.size)
+                })
             }
-            
-            Button(action: {
-                // TODO: Add functionality to add a new workout to the split
-            }) {
-                Text("Add Workout")
-            }
-            .padding(.bottom)
+            .frame(width: fullView.size.width, height: cardHeight, alignment: .center)
+            .position(x: fullView.size.width / 2, y: fullView.size.height / 2)
         }
-        .frame(width: 200)
-        .background(Color.white)
-        .shadow(radius: 5)
-        .padding()
+    }
+    
+    func zIndex(for cardPosition: CGFloat, in fullViewWidth: CGFloat) -> Double {
+        let relativePosition = cardPosition - (fullViewWidth / 2) + (cardWidth / 2)
+        return relativePosition < 0 ? 0 : 1
+    }
+    
+    func handleScrollEnd(with fullViewSize: CGSize) {
+        let approximateCardWidth = cardWidth + spacing
+        let offset = scrollOffset + approximateCardWidth / 2 + fullViewSize.width / 2
+        let cardIndex = round(offset / approximateCardWidth)
+        
+        withAnimation {
+            self.scrollOffset = cardIndex * -approximateCardWidth
+        }
     }
 }
 
