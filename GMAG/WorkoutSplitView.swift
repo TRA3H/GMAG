d919d7c412eb92aff16ba8335d6cb097982596ba
Cyrus Baybay <41849568+TRA3H@users.noreply.github.com>
2023-10-29 00:05:37 -0700

    Update WorkoutSplitView.swift

--- GMAG/WorkoutSplitView.swift
+++ GMAG/WorkoutSplitView.swift
@@ -1,50 +1,125 @@
 import SwiftUI
 
 struct WorkoutSplitView: View {
-    @State private var scrollOffset: CGFloat = 0
-    private let cardWidth: CGFloat = 250
+    @State private var currentIndex: Int = 0
+    @State private var isAddingSplit: Bool = false
+    @GestureState private var dragOffset: CGFloat = 0
+    private let cardWidth: CGFloat = 275
     private let cardHeight: CGFloat = 500
     private let spacing: CGFloat = 30
     
     var body: some View {
         GeometryReader { fullView in
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: spacing) {
-                    ForEach(DayOfWeek.allCases, id: \.self) { day in
-                        GeometryReader { geometry in
-                            Text(day.rawValue)
-                                .frame(width: cardWidth, height: cardHeight)
-                                .background(Color.gray.opacity(0.2))
-                                .cornerRadius(10)
-                                .zIndex(self.zIndex(for: geometry.frame(in: .global).minX, in: fullView.size.width))
-                        }
+            ZStack {
+                ForEach(DayOfWeek.allCases.indices, id: \.self) { index in
+                    Text(DayOfWeek.allCases[index].rawValue)
                         .frame(width: cardWidth, height: cardHeight)
-                    }
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(25)
+                        .opacity(currentIndex == index ? 1.0 : 0.5)
+                        .scaleEffect(currentIndex == index ? 1.2 : 0.9)
+                        .offset(x: CGFloat(index - currentIndex) * (cardWidth + spacing) + dragOffset, y: 0)
                 }
-                .padding(.horizontal, (fullView.size.width - cardWidth) / 2)
-                .offset(x: self.scrollOffset)
-                .gesture(DragGesture().onEnded { value in
-                    self.handleScrollEnd(with: fullView.size)
-                })
             }
+            .gesture(
+                DragGesture()
+                    .updating($dragOffset, body: { (value, state, _) in
+                        state = value.translation.width
+                    })
+                    .onEnded({ value in
+                        let threshold: CGFloat = 50
+                        if value.translation.width > threshold {
+                            withAnimation {
+                                currentIndex = (currentIndex - 1 + DayOfWeek.allCases.count) % DayOfWeek.allCases.count
+                            }
+                        } else if value.translation.width < -threshold {
+                            withAnimation {
+                                currentIndex = (currentIndex + 1) % DayOfWeek.allCases.count
+                            }
+                        }
+                    })
+            )
             .frame(width: fullView.size.width, height: cardHeight, alignment: .center)
             .position(x: fullView.size.width / 2, y: fullView.size.height / 2)
+            .overlay(
+                HStack {
+                    Button(action: {
+                        withAnimation {
+                            currentIndex = (currentIndex - 1 + DayOfWeek.allCases.count) % DayOfWeek.allCases.count
+                        }
+                    }) {
+                        Image(systemName: "arrow.left")
+                            .font(.title)
+                            .padding()
+                    }
+                    Spacer()
+                    Button(action: {
+                        isAddingSplit.toggle()
+                    }) {
+                        Text("+ Add New Split")
+                            .font(.title2)
+                            .padding()
+                    }
+                    Spacer()
+                    Button(action: {
+                        withAnimation {
+                            currentIndex = (currentIndex + 1) % DayOfWeek.allCases.count
+                        }
+                    }) {
+                        Image(systemName: "arrow.right")
+                            .font(.title)
+                            .padding()
+                    }
+                }
+                .padding(.horizontal, 30)
+                .padding(.bottom, 20)
+                , alignment: .bottom
+            )
+            .sheet(isPresented: $isAddingSplit) {
+                AddWorkoutSplitView()
+            }
         }
     }
+}
+
+struct AddWorkoutSplitView: View {
+    @State private var selectedMuscleGroup1: String = "Chest"
+    @State private var selectedMuscleGroup2: String = "Back"
     
-    func zIndex(for cardPosition: CGFloat, in fullViewWidth: CGFloat) -> Double {
-        let relativePosition = cardPosition - (fullViewWidth / 2) + (cardWidth / 2)
-        return relativePosition < 0 ? 0 : 1
+    let muscleGroups = ["Chest", "Back", "Legs", "Shoulders", "Biceps", "Triceps"]
+    
+    var availableMuscleGroups1: [String] {
+        muscleGroups.filter { $0 != selectedMuscleGroup2 }
     }
     
-    func handleScrollEnd(with fullViewSize: CGSize) {
-        let approximateCardWidth = cardWidth + spacing
-        let offset = scrollOffset + approximateCardWidth / 2 + fullViewSize.width / 2
-        let cardIndex = round(offset / approximateCardWidth)
-        
-        withAnimation {
-            self.scrollOffset = cardIndex * -approximateCardWidth
+    var availableMuscleGroups2: [String] {
+        muscleGroups.filter { $0 != selectedMuscleGroup1 }
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Picker("Muscle Group 1", selection: $selectedMuscleGroup1) {
+                    ForEach(availableMuscleGroups1, id: \.self) {
+                        Text($0)
+                    }
+                }
+                Text("/")
+                Picker("Muscle Group 2", selection: $selectedMuscleGroup2) {
+                    ForEach(availableMuscleGroups2, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+            .pickerStyle(WheelPickerStyle())
+            .frame(height: 150)
+            
+            Button("Add New Workout") {
+                // Handle adding a new workout
+            }
+            .padding()
         }
+        .padding()
     }
 }
 
